5620b672e04bc7579236c298459af8adf1431ea1
Kaligula1101 <ilya.kalganov.97@mail.ru>
2021-09-17 23:56:23 +0300

    Initial Commit

---
+++ RWFreeAPI/RWFreeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RWFreeAPIApp.swift
+//  RWFreeAPI
+//
+//  Created by Илья Калганов on 17.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct RWFreeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
