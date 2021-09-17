68f4b57c217a1de5cd08cb6d87a31440c9ef7658
Kaligula1101 <ilya.kalganov.97@mail.ru>
2021-09-18 00:24:41 +0300

    Repack

--- RWFreeAPI/RWFreeAPIApp.swift
+++ RWFreeAPI/RWFreeAPIApp.swift
@@ -1,17 +1,13 @@
-//
-//  RWFreeAPIApp.swift
-//  RWFreeAPI
-//
-//  Created by Илья Калганов on 17.09.2021.
-//
-
 import SwiftUI
 
 @main
 struct RWFreeAPIApp: App {
+    @StateObject private var store = EpisodeStore()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(store)
         }
     }
 }
